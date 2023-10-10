acd0fc483cc37b770837974b42e21f531562ef38
Tatyana Afanasieva <vlakartan@yandex.ru>
2023-10-10 20:17:57 +0300

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,28 @@
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;
+  }
+
+  @Parameterized.Parameters
+  public static Object[][] getTextData() {
+	return new Object[][] {
+		{19, true},
+        {17, false},
+        {18, true},
+        {21, true},
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+    boolean isAdult = program.checkIsAdult(age);
+    assertEquals("Пользователь должен быть совершеннолетним", result, isAdult);
+	}
+}
